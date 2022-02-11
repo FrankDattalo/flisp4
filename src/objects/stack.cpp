20fd261ce3a075c62f478791c07075f8ec218aa9
Frank Dattalo <fjdattalo@gmail.com>
2022-02-10 19:34:35 -0800

    Cleanup

--- src/objects/stack.cpp
+++ src/objects/stack.cpp
@@ -4,13 +4,14 @@
 namespace runtime {
 
 void Stack::Push(Heap* heap, Primitive obj) {
-    Handle obj_handle = heap->GetHandle();
-    Handle this_handle = heap->GetHandle();
+    Handle obj_handle = heap->GetHandle(obj);
+    Handle this_handle = heap->GetHandle(Primitive::Reference(this));
 
-    obj_handle = obj;
-    this_handle = Primitive::Reference(this);
+    Pair* new_head = heap->NewPair(
+        obj_handle.GetData(), this->head()
+    );
 
-    this->head() = Primitive::Reference(heap->NewPair(obj_handle.GetData(), this->head()));
+    this->head() = Primitive::Reference(new_head);
     this->size() = Primitive::Integer(this->size().GetInteger() + 1);
 }
 
