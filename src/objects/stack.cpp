2402cce987a1ea035cb3b585a2204ad1b920330a
Frank Dattalo <fjdattalo@gmail.com>
2022-02-17 21:24:39 -0800

    Cleanup

--- src/objects/stack.cpp
+++ src/objects/stack.cpp
@@ -1,20 +1,6 @@
 #include "objects/stack.hh"
 #include "heap.hh"
 
-/*
-void Stack::Push(Stack* self_, Heap* heap, Primitive obj_) {
-    PrimitiveHandle obj = heap->GetHandle(obj_);
-    ReferenceHandle<Stack> self = heap->GetHandle(self_);
-
-    Pair* new_head = heap->NewPair(
-        obj.GetData(), self->head()
-    );
-
-    self->head() = Primitive::Reference(new_head);
-    self->size() = Primitive::Integer(self->size().GetInteger() + 1);
-}
-*/
-
 void Stack::Push(Heap* heap, Handle stack, Handle item) {
     stack.AsStack()->Head() = heap->NewPair(
         item,
